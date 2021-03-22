62dc7e6a8550b6f25c7ce840df5f6969a57d66c8
wenbo2333 <46727660+wenbo2333@users.noreply.github.com>
2021-03-22 15:22:50 -0400

    first one

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -70,8 +70,17 @@ class NEURON:
         
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
         
-    def Update_Hidden_Or_Motor_Neuron(self):
-        self.Set_Value(math.pi/4.0)
+    def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
+        self.Set_Value(0)
+        for s in synapses:
+            if self.Get_Name() == s[1]:
+                self.Allow_Presynaptic_Neuron_To_Influence_Me(synapses[s].Get_Weight(), neurons[s[0]].Get_Value())
+        self.Threshold()
+        
+    
+    def Allow_Presynaptic_Neuron_To_Influence_Me(self, weight, presyn_value):
+        x = presyn_value * weight
+        self.Add_To_Value(x)
 
 # -------------------------- Private methods -------------------------
 
